aca0a5eba1aada29fff9444138619f581427b1b1
u7knv9h <u7knv9h@openmailbox.org>
2015-04-10 19:18:20 +0200

    consider cached memory

--- infornography.sh
+++ infornography.sh
@@ -23,13 +23,15 @@ BAT="battery: $(acpi | awk '{print $4}')"
 case $OS in
     *BSD)
         CPU="$(sysctl -n hw.model)"
-        UPTIME="$(uptime | awk '{gsub(/,/,""); print ($3, $4)}')"
+        UPTIME="$(uptime | awk '{gsub(/,/,"") print $3 $4)}')"
         MEM="$(top -n 1 -b | awk '/Memory/{print $3}')" 
         if test -z "$MEM"; then
             if test -f /proc/meminfo; then
                 MEMF="$(awk '/MemFree/{print int($2/10^3)}' /proc/meminfo)"
+    		MEMC="$(awk '/Cached/{print int($2/10^3)}' /proc/meminfo)"
+	        MEMA="$(( MEMF+MEMC ))"
                 MEMT="$(awk '/MemTotal/{print int($2/10^3)}' /proc/meminfo)"
-                MEM="$(( MEMT-MEMF ))/${MEMT}M"
+                MEM="$(( MEMT-MEMA ))/${MEMT}M"
             else
                 MEMT="$(vmstat -h | awk 'NR==3{print $4}')"
                 MEMF="$(vmstat -h | awk 'NR==3{gsub(/M/,""); print $5}')"
@@ -43,12 +45,15 @@ case $OS in
         T="$(awk '{print int($1)}' /proc/uptime)"
 	UPTIME="$(printf '%dd %dh %dm\n' \
 		$(( $T/86400 )) $(( $T%86400/3600 )) $(( $T%3600/60 )))"
-        MEMF="$(awk '/MemAvailable/{print int($2/10^3)}' /proc/meminfo)"
-        if test -z "$MEMF"; then
+        MEMA="$(awk '/MemAvailable/{print int($2/10^3)}' /proc/meminfo)"
+        if test -z "$MEMA"; then
             MEMF="$(awk '/MemFree/{print int($2/10^3)}' /proc/meminfo)"
-        fi
+	    MEMC="$(awk '/Cached/{print int($2/10^3)}' /proc/meminfo)"
+	    MEMA="$(( MEMF+MEMC ))"
+        
+fi
         MEMT="$(awk '/MemTotal/{print int($2/10^3)}' /proc/meminfo)"
-        MEM="$(( MEMT-MEMF ))/${MEMT}M"
+        MEM="$(( MEMT-MEMA ))/${MEMT}M"
         ;;
     *)
         ;;
@@ -77,8 +82,7 @@ cat << EOF
  .....................x.............  
  ...................................  
 ..................................... 
-..................................... 
- ......................x............. 
+.......................x............. 
 EOF
 
-unset MEM MEMF MEMT BAT CPU UPTIME VERS OS COLOR
+unset MEM MEMF MEMA MEMC MEMT BAT CPU UPTIME VERS OS COLOR
